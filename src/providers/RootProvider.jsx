5480df60d85505bcfe6338787a84928e7876c7f3
“izawasoma” <izawasoma@gmail.com>
2024-12-21 11:15:09 +0900

    メニューが初期状態でopenになっているのを修正

--- src/providers/RootProvider.jsx
+++ src/providers/RootProvider.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from "react";
 export const RootContext = createContext(null);
 
 export const RootProvider = (props) => {
-    const [hamburgerMenuState,setHamburgerMenuState] = useState(true);
+    const [hamburgerMenuState,setHamburgerMenuState] = useState(false);
     return (
         <RootContext.Provider value={{ hamburgerMenuState ,setHamburgerMenuState }}>
             { props.children }
